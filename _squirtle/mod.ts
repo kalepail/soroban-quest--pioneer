cf19c1e8717de9b869f512d2f3590b87929ec08a
Tyler van der Hoeven <hi@tyvdh.com>
2022-11-17 01:40:08 +0000

    fixed a little spacing issue

--- _squirtle/mod.ts
+++ _squirtle/mod.ts
@@ -123,7 +123,7 @@ const runUser = async (
   let missing = false
 
   if (user.isBlocked)
-    console.log(`   ❌ ${user.isBlocked === user.pk ? '' : 'Previous'} Stellar wallet (${user.isBlocked}) has been flagged`);
+    console.log(`   ❌${user.isBlocked === user.pk ? '' : 'Previous'} Stellar wallet (${user.isBlocked}) has been flagged`);
   else if (user.pk)
     console.log(`   ✅ Stellar wallet ${user.pk.substring(0, 6)}...${user.pk.substring(user.pk.length - 6)} is connected`);
   else {
